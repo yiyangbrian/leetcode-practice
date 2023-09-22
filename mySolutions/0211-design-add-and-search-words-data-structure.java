c76680bd5e8e4c71f8117c89a83ee2dd0f9267eb
Yiyang Huang <huangyiyang1996@gmail.com>
2023-09-22 16:20:13 +0800

    Sep 22

--- mySolutions/0211-design-add-and-search-words-data-structure.java
+++ mySolutions/0211-design-add-and-search-words-data-structure.java
@@ -1,41 +1,3 @@
-class WordDictionary {
-    private Trie root;
-
-    public WordDictionary() {
-        root = new Trie();
-    }
-
-    public void addWord(String word) {
-        root.insert(word);
-    }
-
-    public boolean search(String word) {
-        return dfs(word, 0, root);
-    }
-
-    private boolean dfs(String word, int index, Trie node) {
-        if(index == word.length()) {
-            return node.isEnd();
-        }
-        char ch = word.charAt(index);
-        if(Character.isLetter(ch)) {
-            int childIndex = ch - 'a';
-            Trie child = node.getChildren()[childIndex];
-            if(child != null && dfs(word, index + 1, child)) {
-                return true;
-            }
-        } else {
-            for(int i = 0; i < 26; i ++) {
-                Trie child = node.getChildren()[i];
-                if(child != null && dfs(word, index + 1, child)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-}
-
 class Trie {
     private Trie[] children;
     private boolean isEnd;
@@ -62,8 +24,44 @@ class Trie {
         return children;
     }
 
-    public boolean isEnd() {
+    public boolean getIsEnd() {
         return isEnd;
+    }
+}
+
+class WordDictionary {
+    private Trie node;
+
+    public WordDictionary() {
+        node = new Trie();
+    }
+
+    public void addWord(String word) {
+        node.insert(word);
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, node);
+    }
+
+    public boolean dfs(String word, int index, Trie node) {
+        if(index == word.length()) {
+            return node.getIsEnd();
+        }
+        char ch = word.charAt(index);
+        if(Character.isLetter(ch)) {
+            int childIndex = ch - 'a';
+            if(node.getChildren()[childIndex] != null && dfs(word, index + 1, node.getChildren()[childIndex])) {
+                return true;
+            }
+        } else {
+            for(int i = 0; i < 26; i ++) {
+                if(node.getChildren()[i] != null && dfs(word, index + 1, node.getChildren()[i])) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
