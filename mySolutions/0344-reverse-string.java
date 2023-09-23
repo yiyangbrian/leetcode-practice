bfeb49f797e67ad1db5ad34cb3f2539180077154
Yiyang Huang <huangyiyang1996@gmail.com>
2023-09-23 19:20:09 +0800

    Sep 23

---
+++ mySolutions/0344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    private void helper(char[] s, int start, int end) {
+        if(start >= end) {
+            return;
+        }
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        helper(s, start + 1, end - 1);
+    }
+    public void reverseString(char[] s) {
+        helper(s, 0, s.length - 1);
+    }
+}
