bd7009448d8741fa132ab9485ccf5114388594ed
Yiyang Huang <huangyiyang1996@gmail.com>
2023-11-16 12:28:09 +0800

    Nov 16

--- mySolutions/1631-path-with-minimum-effort-dijkstra.java
+++ mySolutions/1631-path-with-minimum-effort-dijkstra.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -5,52 +6,42 @@ class Solution {
     public int minimumEffortPath(int[][] heights) {
         int rows = heights.length;
         int columns = heights[0].length;
-
-        int[][] effort = new int[rows][columns];
+        int n = rows * columns;
         int[][] visited = new int[rows][columns];
+        int[][] distances = new int[rows][columns];
 
-        Queue<Integer> pq = new PriorityQueue<>((a, b) -> effort[a / columns][a % columns] - effort[b / columns][b % columns]);
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> distances[a / columns][a % columns] - distances[b / columns][b % columns]);
 
-        for(int[] row : effort) {
-            Arrays.fill(row, Integer.MAX_VALUE);
+        pq.offer(0);
+        for(int[] distance : distances) {
+            Arrays.fill(distance, Integer.MAX_VALUE);
         }
-        effort[0][0] = 0;
-        pq.add(0);
+        distances[0][0] = 0;
 
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
         while(!pq.isEmpty()) {
-            int cur = pq.poll();
-
-            int row = cur / columns;
-            int column = cur % columns;
-
-            visited[row][column] = 1;
-            int curValue = heights[row][column];
+            int curNode = pq.poll();
+            int row = curNode / columns, col = curNode % columns;
+            visited[row][col] = 1;
 
             for(int[] direction : directions) {
-                // neighbor -> heights[i][j];
                 int i = row + direction[0];
-                int j = column + direction[1];
+                int j = col + direction[1];
                 int idx = i * columns + j;
 
-                if(i >= 0 && i < rows && j >= 0 && j < columns && visited[i][j] != 1) {
-
-                    int neighborValue = heights[i][j];
-                    int curEffort = Math.abs(curValue - neighborValue);
-                    int maxEffort = Math.max(curEffort, effort[row][column]);
-
-                    if(maxEffort < effort[i][j]) {
-                        effort[i][j] = maxEffort;
-                        if(pq.contains(idx)) {
-                            pq.remove(idx);
-                        }
-                        pq.add(idx);
+                if(i >= 0 && i < rows && j >= 0 && j < columns && visited[i][j] == 0) {
+                    int curEffort = Math.abs(heights[row][col] - heights[i][j]);
+                    int curMaxEffort = Math.max(curEffort, distances[row][col]);
+                    if(curMaxEffort < distances[i][j]) {
+                        distances[i][j] = curMaxEffort;
+                        pq.remove(idx);
+                        pq.offer(idx);
                     }
                 }
             }
         }
 
-        return effort[rows - 1][columns - 1];
+        return distances[rows - 1][columns - 1];
     }
 }
